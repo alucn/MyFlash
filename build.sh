7ffeb01ae11df441de3b439cb2a26a6305b2f881
root <root@dp.wangguangyou>
2017-11-01 04:47:07 +0800

    add customized libmysqlclient

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+ln -s binary/libmysqlclient.so.20.3.7 /usr/local/mysql/lib/libmysqlclient.so
 export LD_LIBRARY_PATH=/usr/local/mysql/lib/
-gcc -g -w  `pkg-config --cflags --libs glib-2.0` -I/usr/local/mysql/include -L/usr/local/mysql/lib/ -lmysqlclient source/binlogParseGlib.c source/mysqlHelper/common.c  source/mysqlHelper/dumpFromRemote.c  -o binary/flashback
+ gcc -g -w  `pkg-config --cflags --libs glib-2.0` `pkg-config --cflags mysqlclient` -L/usr/local/mysql/lib/ -lmysqlclient source/binlogParseGlib.c source/mysqlHelper/common.c  source/mysqlHelper/dumpFromRemote.c  -o binary/flashback
