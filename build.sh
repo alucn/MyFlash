9c8b6b8b936c23bb1bc14819d0ca4fa0c7d0d887
root <root@dp.wangguangyou>
2017-11-01 04:51:01 +0800

    fix install

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p /usr/local/mysql/lib/
 ln -s binary/libmysqlclient.so.20.3.7 /usr/local/mysql/lib/libmysqlclient.so
 export LD_LIBRARY_PATH=/usr/local/mysql/lib/
  gcc -g -w  `pkg-config --cflags --libs glib-2.0` `pkg-config --cflags mysqlclient` -L/usr/local/mysql/lib/ -lmysqlclient source/binlogParseGlib.c source/mysqlHelper/common.c  source/mysqlHelper/dumpFromRemote.c  -o binary/flashback
--- doc/INSTALL.md
+++ doc/INSTALL.md
@@ -2,8 +2,6 @@
 该工具推荐用户在下载源码之后，进行动态编译链接安装
 
 #### 动态编译链接
-flashback利用MySQL的源码，提供了远程回滚功能。然而相关API并没有在原生的limysqlclient中导出。因此如果要顺利编译和运行，需要到该地址进行编译安装(https://github.com/mark-neil-wang/mysql57-flashback-related) 之后（安装方法参考MySQL文档http://dev.mysql.com/doc/internals/en/cmake-howto-quick-debug-configuration.html）
-再运行如下命令安装flashback软件
 
 ```
 sh build.sh
